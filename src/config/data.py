a7bab23e5507ad45370d0aeb849094f338f6e98d
nikmosi <i@nikmosi.ru>
2025-09-26 19:53:35 +0700

    feat: add aiolimiter

--- src/config/data.py
+++ src/config/data.py
@@ -11,9 +11,15 @@ class TrimSettings(BaseModel):
     interactive: bool
 
 
+class LimiterSettings(BaseModel):
+    max_rate: float
+    time_period: float
+
+
 class Settings(BaseSettings):
     working_directory: Path = Path(".")
     chunk_size: int = 40
     url: URL
     trim_args: TrimSettings
     saver: type
+    limiter: LimiterSettings
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import asyncio
 import contextlib
 from collections.abc import Awaitable
 
+from aiolimiter import AsyncLimiter
 from dependency_injector.wiring import Provide, inject
 from loguru import logger
 from pipe import batched
@@ -12,7 +13,6 @@ from core import ChapterLoader, MainPageLoader, SaverLoaderConnector
 from domain import SaverContext
 from utils import (
     change_working_directory,
-    parse_console_arguments,
     trim,
 )
 
@@ -22,6 +22,7 @@ async def run(
     args: Settings,
     main_page_loader: MainPageLoader,
     chapter_loader: ChapterLoader,
+    limiter: AsyncLimiter,
 ):
     main_page = await main_page_loader.load()
     trimmed_chapters = trim(args.trim_args, main_page.chapters)
@@ -34,19 +35,21 @@ async def run(
         for chunked in trimmed_chapters | batched(args.chunk_size):
             async with asyncio.TaskGroup() as tg:
                 for chapter in chunked:
-                    tg.create_task(connector.handle(chapter))
+                    async with limiter:
+                        tg.create_task(connector.handle(chapter))
 
 
 @inject
 async def main(
+    args: Settings = Provide[Container.settings],
+    limiter: AsyncLimiter = Provide[Container.limiter],
     loader_service: LoaderService = Provide[Container.loader_service],
 ):
     logger.debug("run")
-    args = parse_console_arguments()
     change_working_directory(args.working_directory)
     loader = loader_service.get(args.url)
     chapter_loader = loader.get_loader_for_chapter()
-    await run(args, loader, chapter_loader)
+    await run(args, loader, chapter_loader, limiter=limiter)
     logger.info("done")
 
 

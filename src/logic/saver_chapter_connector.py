3aebad9f7b985bd7d1dd242fb992990ff92cddf8
nikmosi <i@nikmosi.ru>
2025-11-08 13:50:49 +0700

    feat(saver_chapter_connector): add retries

--- src/logic/saver_chapter_connector.py
+++ src/logic/saver_chapter_connector.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
+from datetime import timedelta
 
 from loguru import logger
+from tenacity import AsyncRetrying, stop_after_attempt, wait_chain, wait_fixed
 
 from domain import Chapter
 from logic.loader import ChapterLoader
@@ -13,6 +15,28 @@ class SaverLoaderConnector:
     chapter_loader: ChapterLoader
 
     async def handle(self, chapter: Chapter):
-        logger.debug(f"{chapter.base_name}")
-        loaded_chapter = await self.chapter_loader.load_chapter(chapter)
-        await self.saver.save_chapter(loaded_chapter)
+        logger.debug(f"working with {chapter.base_name}")
+        async for attempt in AsyncRetrying(
+            stop=stop_after_attempt(5),
+            wait=wait_chain(
+                wait_fixed(timedelta(seconds=5))
+                + wait_fixed(timedelta(seconds=10))
+                + wait_fixed(timedelta(seconds=30))
+                + wait_fixed(timedelta(seconds=60))
+                + wait_fixed(timedelta(seconds=120))
+            ),
+            reraise=True,
+        ):
+            with attempt:
+                attempt_number = attempt.retry_state.attempt_number
+                logger.info(f"[Try {attempt_number}] loading {chapter.base_name}")
+                try:
+                    loaded_chapter = await self.chapter_loader.load_chapter(chapter)
+                    await self.saver.save_chapter(loaded_chapter)
+                # TODO: make more specific errors for loading, parsing, saving
+                except Exception as e:
+                    logger.warning(
+                        f"⚠️ Ошибка при обработке {chapter.base_name}: {e!r} "
+                        f"(попытка {attempt.retry_state.attempt_number})"
+                    )
+                    raise

3be9da6ba640d062dc02dc1fdf2d20c5868ea8f6
nikmosi <i@nikmosi.ru>
2025-11-14 16:04:00 +0700

    feat(saver): import all from infra.saver before finding savers subclasses

--- src/utils/saver.py
+++ src/utils/saver.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import importlib
+import pkgutil
 from collections.abc import Iterable
+from functools import lru_cache
 from typing import TypeVar
 
-from infra.saver import EbookSaver, FilesSaver
+from infra import saver as saver_pkg
 from logic import Saver
 
 from .exceptions import FindSaverError
@@ -11,7 +14,16 @@ from .exceptions import FindSaverError
 SaverType = TypeVar("SaverType", bound=Saver)
 
 
+@lru_cache(1)
+def import_all_infra_savers() -> None:
+    for module_info in pkgutil.iter_modules(
+        saver_pkg.__path__, saver_pkg.__name__ + "."
+    ):
+        importlib.import_module(module_info.name)
+
+
 def inheritors(klass: type[SaverType]) -> set[type[SaverType]]:
+    import_all_infra_savers()
     subclasses: set[type[SaverType]] = set()
     work: list[type[SaverType]] = [klass]
     while work:

1f109e4cc67d2fa406a8f7487e10bb6f5cf7c345
nikmosi <nikmosvok@gmail.com>
2025-11-14 16:35:06 +0700

    Improve main page parsers error handling

--- src/infra/main_page/ranobes.py
+++ src/infra/main_page/ranobes.py
@@ -9,6 +9,11 @@ from yarl import URL
 
 from domain import Chapter, LoadedChapter, MainPageInfo
 from domain.images import Image
+from infra.main_page.exceptions import (
+    EmptyChapterContentError,
+    MainPageParsingError,
+    PaginationParsingError,
+)
 from logic import ChapterLoader, MainPageLoader
 from utils.bs4 import get_soup
 
@@ -21,16 +26,22 @@ class RanobesChapterLoader(ChapterLoader):
         res = await get_soup(self.session, chapter.url)
         container = res.find("div", id="dle-content")
         if container is None:
-            raise ValueError(container)
+            raise MainPageParsingError(detail="chapter content container missing", page_url=chapter.url)
 
-        title = container.find("h1").get_text()  # pyright: ignore
+        title_tag = container.find("h1")
+        if title_tag is None:
+            raise MainPageParsingError(detail="chapter title not found", page_url=chapter.url)
+        title = title_tag.get_text()
 
         all_p_tags = container.find_all("p")
         paragraphs = [i.text for i in all_p_tags]
         if not paragraphs:
             article = container.find("div", id="arrticle", class_="text")
             if not article:
-                raise ValueError(f"{article=} and {paragraphs=}")
+                raise EmptyChapterContentError(
+                    detail="ranobes returned empty chapter",
+                    page_url=chapter.url,
+                )
             text = article.get_text("\n")
             text = re.sub(r"\n{2,}", "\n", text)
             paragraphs = text.split("\n")
@@ -56,26 +67,38 @@ class RanobesLoader(MainPageLoader):
 
         chapter_tag = main_page_soup.find("div", class_="r-fullstory-chapters-foot")
         if not chapter_tag:
-            raise ValueError(chapter_tag)
+            raise MainPageParsingError(detail="chapter block not found", page_url=self.url)
 
-        title = str(main_page_soup.find("h1", class_="title").text)  # pyright: ignore
+        title_tag = main_page_soup.find("h1", class_="title")
+        if not title_tag:
+            raise MainPageParsingError(detail="title tag missing", page_url=self.url)
+        title = str(title_tag.text)
         if not title:
-            raise ValueError(title)
+            raise MainPageParsingError(detail="empty title", page_url=self.url)
 
-        chapter_page_url = URL(chapter_tag.find("a").find_next("a").get("href"))  # pyright: ignore
+        first_link = chapter_tag.find("a")
+        if first_link is None:
+            raise MainPageParsingError(detail="chapter page link not found", page_url=self.url)
+        next_link = first_link.find_next("a")
+        if next_link is None:
+            raise MainPageParsingError(detail="chapter page next link missing", page_url=self.url)
+        next_href = next_link.get("href")
+        if not next_href:
+            raise MainPageParsingError(detail="chapter page href missing", page_url=self.url)
+        chapter_page_url = URL(next_href)
         if not chapter_page_url.is_absolute():
             chapter_page_url = self.url.with_path(str(chapter_page_url))
-        if not chapter_page_url:
-            raise ValueError(chapter_page_url)
 
-        image_path = URL(
-            main_page_soup.find("div", class_="r-fullstory-poster")
-            .find("img")  # pyright: ignore
-            .get("src")  # pyright: ignore
-        )
-
-        if not image_path:
-            raise ValueError(chapter_page_url)
+        image_container = main_page_soup.find("div", class_="r-fullstory-poster")
+        if image_container is None:
+            raise MainPageParsingError(detail="cover container missing", page_url=self.url)
+        image_tag = image_container.find("img")
+        if image_tag is None:
+            raise MainPageParsingError(detail="cover image missing", page_url=self.url)
+        image_src = image_tag.get("src")
+        if not image_src:
+            raise MainPageParsingError(detail="cover image src missing", page_url=self.url)
+        image_path = URL(image_src)
 
         image = Image(url=self.url.with_path(str(image_path)))
         loaded_image = await self.image_loader.load_image(image)
@@ -93,17 +116,26 @@ class RanobesLoader(MainPageLoader):
         logger.debug("collect pages")
         pages = chapters_page.find("div", class_="pages")
         if pages is None:
-            raise ValueError(pages)
+            raise PaginationParsingError(detail="pages container missing", page_url=self.url)
 
         pages_with_num: dict[int, str] = {}
         for i in pages.find_all("a"):
-            num = int(i.get_text())
+            try:
+                num = int(i.get_text())
+            except ValueError as exc:
+                raise PaginationParsingError(
+                    detail="cannot convert pagination number to int",
+                    page_url=self.url,
+                ) from exc
             href = i.get("href")
 
             if href is None:
-                raise ValueError(href)
+                raise PaginationParsingError(detail="pagination link without href", page_url=self.url)
 
             pages_with_num[num] = str(href)
+
+        if not pages_with_num:
+            raise PaginationParsingError(detail="pagination list is empty", page_url=self.url)
 
         max_page_num = max(pages_with_num.keys())
         max_page = pages_with_num[max_page_num]
@@ -124,12 +156,12 @@ class RanobesLoader(MainPageLoader):
             soup = await get_soup(self.session, page)
             container = soup.find("div", id="dle-content")  # pyright.ignore
             if container is None:
-                raise ValueError(container)
+                raise MainPageParsingError(detail="chapter list container missing", page_url=page)
             lines = container.find_all("div", class_="cat_line")
             for line in reversed(lines):
                 link = line.find("a")
                 if link is None:
-                    raise ValueError(link)
+                    raise MainPageParsingError(detail="chapter line missing anchor", page_url=page)
 
                 url = URL(str(link.get("href")))
                 name = str(link.get("title"))
--- src/infra/main_page/renovels.py
+++ src/infra/main_page/renovels.py
@@ -2,13 +2,20 @@ import asyncio
 import json
 from collections.abc import Sequence
 from dataclasses import dataclass
-from typing import override
+from json import JSONDecodeError
+from typing import Any, override
 
 from bs4 import BeautifulSoup
 from loguru import logger
 from yarl import URL
 
 from domain import Chapter, Image, LoadedChapter, MainPageInfo
+from infra.main_page.exceptions import (
+    InvalidJsonFieldError,
+    JsonParsingError,
+    MainPageParsingError,
+    MissingJsonFieldError,
+)
 from logic import ChapterLoader, MainPageLoader
 from utils.bs4 import get_soup, get_text_response
 
@@ -17,11 +24,19 @@ from utils.bs4 import get_soup, get_text_response
 class RenovelsChapterLoader(ChapterLoader):
     @override
     async def load_chapter(self, chapter: Chapter) -> LoadedChapter:
-        res = json.loads(await get_text_response(self.session, chapter.url))
+        raw_response = await get_text_response(self.session, chapter.url)
+        try:
+            res = json.loads(raw_response)
+        except JSONDecodeError as exc:
+            raise JsonParsingError(page_url=chapter.url) from exc
         logger.debug(f"get {chapter.base_name}")
-        content = res["content"]
-        title = content["chapter"]
-        content_p = content["content"]
+        content = _extract_field(res, ["content"], chapter.url)
+        title = _ensure_str(_extract_field(content, ["chapter"], chapter.url), "content.chapter", chapter.url)
+        content_p = _ensure_str(
+            _extract_field(content, ["content"], chapter.url),
+            "content.content",
+            chapter.url,
+        )
         html = BeautifulSoup(content_p, "lxml").find_all("p")
         paragraphs = [i.text for i in html]
 
@@ -45,17 +60,36 @@ class RenovelsLoader(MainPageLoader):
         main_page_soup = await get_soup(self.session, self.url)
         scripts = main_page_soup.find(id="__NEXT_DATA__")
         if scripts is None:
-            raise Exception("can't get __NEXT_DATA__.")
-        data = json.loads(scripts.get_text())
+            raise MainPageParsingError(detail="__NEXT_DATA__ script not found", page_url=self.url)
+        try:
+            data = json.loads(scripts.get_text())
+        except JSONDecodeError as exc:
+            raise JsonParsingError(page_url=self.url) from exc
 
         # TODO: create model for response
-        content = data["props"]["pageProps"]["fallbackData"]["content"]
-        branches = content["branches"][0]
-        img_path = content["img"]["high"]
-        branch_id = int(branches["id"])
-        count_chapters = content["count_chapters"]
+        content = _extract_field(data, ["props", "pageProps", "fallbackData", "content"], self.url)
+        branches = _extract_field(content, ["branches"], self.url)
+        if not isinstance(branches, list) or not branches:
+            raise MissingJsonFieldError(field_path="content.branches", page_url=self.url)
+        branch_info = branches[0]
+        if not isinstance(branch_info, dict):
+            raise InvalidJsonFieldError(
+                field_path="content.branches[0]",
+                expected="object",
+                page_url=self.url,
+            )
+        img = _extract_field(content, ["img"], self.url)
+        if not isinstance(img, dict):
+            raise InvalidJsonFieldError(field_path="content.img", expected="object", page_url=self.url)
+        img_path = _ensure_str(img.get("high"), "content.img.high", self.url)
+        branch_id = _ensure_int(branch_info.get("id"), "content.branches[0].id", self.url)
+        count_chapters = _ensure_int(
+            _extract_field(content, ["count_chapters"], self.url),
+            "content.count_chapters",
+            self.url,
+        )
 
-        title = content["main_name"]
+        title = _ensure_str(_extract_field(content, ["main_name"], self.url), "content.main_name", self.url)
         image = Image(self.domain.with_path(img_path))
         cover = await self.image_loader.load_image(image)
 
@@ -85,7 +119,51 @@ class RenovelsLoader(MainPageLoader):
                         )
                     )
                 )
-        results = [json.loads(i.result())["content"] for i in tasks]
-        ids = [j["id"] for i in results for j in i]
+        results: list[list[dict[str, Any]]] = []
+        for task in tasks:
+            try:
+                payload = json.loads(task.result())
+            except JSONDecodeError as exc:
+                raise JsonParsingError(page_url=self.url) from exc
+            content = payload.get("content")
+            if not isinstance(content, list):
+                raise MissingJsonFieldError(field_path="content", page_url=self.url)
+            list_content: list[dict[str, Any]] = []
+            for chapter_data in content:
+                if not isinstance(chapter_data, dict):
+                    raise InvalidJsonFieldError(
+                        field_path="content[]",
+                        expected="object",
+                        page_url=self.url,
+                    )
+                list_content.append(chapter_data)
+            results.append(list_content)
+
+        ids: list[int] = []
+        for chunks in results:
+            for item in chunks:
+                ids.append(_ensure_int(item.get("id"), "content[].id", self.url))
         api = URL("https://api.renovels.org/api/titles/chapters/")
         return [Chapter(i, str(i), api / str(j)) for i, j in enumerate(ids, 1)]
+
+
+def _extract_field(data: Any, path: Sequence[str], page_url: URL) -> Any:
+    current = data
+    for key in path:
+        if not isinstance(current, dict) or key not in current:
+            raise MissingJsonFieldError(field_path=".".join(path), page_url=page_url)
+        current = current[key]
+    return current
+
+
+def _ensure_str(value: Any, field_path: str, page_url: URL) -> str:
+    if not isinstance(value, str) or not value:
+        raise InvalidJsonFieldError(field_path=field_path, expected="non-empty string", page_url=page_url)
+    return value
+
+
+def _ensure_int(value: Any, field_path: str, page_url: URL) -> int:
+    try:
+        return int(value)
+    except (TypeError, ValueError) as exc:
+        raise InvalidJsonFieldError(field_path=field_path, expected="integer", page_url=page_url) from exc
